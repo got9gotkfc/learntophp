ee89dc065f0788c4c3a6948833572e2784c8465f
Minilin <a0966678326@gmail.com>
2022-04-22 15:28:49 +0800

    字串的取用

--- string.php
+++ string.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>字串練習</h1>
     <article>
-       <h3>字串取代</h3>
-       <div>將"aaddw1123"改成*********</div> 
+        <h3>字串取代</h3>
+        <div>將"aaddw1123"改成*********</div>
     </article>
     <?php
-    $password="aaddw1123";
-    $strlen=mb_strlen($password);
-    $password=str_repeat("*",$strlen);
+    $password = "aaddw1123";
+    $strlen = mb_strlen($password);
+    $password = str_repeat("*", $strlen);
     echo $password;
     ?>
     <article>
@@ -23,8 +25,8 @@
         <div>將"I have a pen"依","切割後成為陣列</div>
     </article>
     <?php
-    $str="I,have,a,pen";
-    $array=explode(",",$str);
+    $str = "I,have,a,pen";
+    $array = explode(",", $str);
     echo "<pre>";
     print_r($array);
     echo "</pre>";
@@ -34,17 +36,27 @@
         <div>將上列陣列組合</div>
     </article>
     <?php
-    $str="I,have,a,pen";
-    $array=explode(",",$str);
+    $str = "I,have,a,pen";
+    $array = explode(",", $str);
     echo "<pre>";
     print_r($array);
     echo "</pre>";
 
-    $newstr=implode("&nbsp",$array);
+    $newstr = implode("&nbsp", $array);
     echo $newstr;
+    ?>
+    <article>
+        <h3>子字串取用</h3>
+        <div>將” The reason why a great man is great is that he resolves to be a great man”只取前十字成為” The reason…”</div>
+    </article>
+    <?php
+    $str = "The reason why a great man is great is that he resolves to be a great man";
+    $newstr=substr($str,0,10);
+    echo $newstr . "...";
 
     ?>
 
 
 </body>
+
 </html>
