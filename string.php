0f318712c71567b85b5ae0860bc3207693a8b990
Minilin <a0966678326@gmail.com>
2022-04-22 16:20:27 +0800

    字串函式整合應用

--- string.php
+++ string.php
@@ -51,10 +51,43 @@
     </article>
     <?php
     $str = "The reason why a great man is great is that he resolves to be a great man";
-    $newstr=substr($str,0,10);
+    $newstr = substr($str, 0, 10);
     echo $newstr . "...";
 
     ?>
+
+    <article>
+        <h3>字串函式整合應用</h3>
+        <div>給定一個句子，將指定的關鍵字放大
+            “學會PHP網頁程式設計，薪水會加倍，工作會好找”
+            請將上句中的 “程式設計” 放大字型或變色.</div>
+    </article>
+    <?php
+    $str = "學會PHP網頁程式設計，薪水會加倍，工作會好找";
+    // $search = "程式設計";  
+    // $pos = mb_strpos($str, $search);
+    // $str2 = mb_substr($str, 0, $pos)
+    //     . "<span style='font-size:2rem;color:red'>"
+    //     . $search
+    //     . "</span>"
+    //     . mb_substr($str, $pos + strlen($search)/3, strlen($str));
+    // echo "<br>";
+    // echo $str2;
+
+    $search="工作";
+    $pos=mb_strpos($str,$search);
+    $head=mb_substr($str,0,$pos);
+    $tail=mb_substr($str,$pos+mb_strlen($search));
+    $newstr=$head
+            ."<span style='font-size:2rem;color:red'>"
+            . $search
+            . "</span>"
+            .$tail;
+            echo $newstr;
+
+    ?>
+
+
 
 
 </body>
